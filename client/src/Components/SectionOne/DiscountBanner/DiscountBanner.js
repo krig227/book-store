8b96a5bc6a87968f5c718df0c7d891b22d9c77d9
krig227 <g.krishna3204@gmail.com>
2023-12-11 18:51:11 +0530

    commit

--- client/src/Components/SectionOne/DiscountBanner/DiscountBanner.js
+++ client/src/Components/SectionOne/DiscountBanner/DiscountBanner.js
@@ -7,7 +7,7 @@ const DiscountBanner = () => {
   const apiUrl =
     process.env.NODE_ENV === "development"
       ? process.env.REACT_APP_API_URL
-      : process.env.REACT_APP_PROD_API_URL;
+      : process.env.REACT_APP_API_URL;
 
   useEffect(() => {
     console.log(apiUrl);
